a14b4b3e6b4f96c9b53327e6c23704076036bf0f
siva12143 <siva121435@gmail.com>
2025-10-14 01:41:44 +0530

    update

--- app/js/action.js
+++ app/js/action.js
@@ -3,22 +3,116 @@
 const tableBody = document.querySelector("#productTable tbody");
 const addRowBtn = document.getElementById("addRowBtn");
 
-addRowBtn.addEventListener("click", () => {
+addRowBtn.addEventListener("click", async () => {
+    let materilCount = document.getElementById("materilCount").innerHTML || 0;
+    console.log(materilCount);
+
+    const config = {
+        appName: "fab-master-erp1",
+        reportName: "All_Products",
+        page: 1,
+        pageSize: 200,
+    };
+    await ZOHO.CREATOR.API.getAllRecords(config).then(async response => {
+        getProducts = await response.data;
+    })
+    console.log(getProducts);
+
+
     const newRow = document.createElement("tr");
     newRow.className = "bg-white border-b border-gray-200 hover:bg-gray-50";
-
     newRow.innerHTML = `
-        <select name="" id="" class="w-[95%] px-3 py-2 font-medium text-gray-900 whitespace-nowrap">
-                                                                    <option value="">Select</option>
-                                                                    <option value="Select 1">Select 1</option>
-                                                                    <option value="Select 2">Select 2</option>
-                                                                    <option value="Select 3">Select 3</option>
-                                                                </select>
-                                                            </td>
-                                                            <td><input class="px-3 py-2 font-medium text-gray-900 whitespace-nowrap" type="number" name="" id=""></td>
+        <td id="rowId${materilCount}">
+            <select name="" id="rowSelect${materilCount}" class="p-[10px]">
+           </select>
+        </td>
+        <td><input class="px-3 py-2 font-medium text-gray-900 whitespace-nowrap" type="number" name="" id=""></td>
       `;
 
+    updateOption = newRow.children[0].children[0];
+    console.log(updateOption);
+    console.log(newRow.children[0].children[0]);
+
+    updateOption.innerHTML = "<option value=''>Select</option>";
+    getProducts.forEach(e => {
+        updateOption.innerHTML += `<option value="${e.ID}">${e.Item_Name}</option>`
+    });
+
     tableBody.appendChild(newRow);
+    materilCount++;
 });
 
 // Job Material table add row End
+
+const updateValue = () => {
+    const ReqId = document.getElementById("Job_Request_ID");
+    const Customer = document.getElementById("Customer");
+    const Service = document.getElementById("Service");
+    const PropertyName = document.getElementById("Property_Name");
+    const PropertyNumber = document.getElementById("Property_Number");
+    const PropertyType = document.getElementById("Property_Type");
+    const PropertyUnit = document.getElementById("Property_Unit");
+    const Address = document.getElementById("Address");
+    const Zone = document.getElementById("Zone");
+    const AccessCode = document.getElementById("Access_Code");
+    const ScheduledDate = document.getElementById("ScheduledDate");
+    const StartTime = document.getElementById("Start_Time");
+    const EndTime = document.getElementById("End_Time");
+    const Crew = document.getElementById("Crew");
+    const Driver = document.getElementById("Driver");
+    const PickupLocation = document.getElementById("Pickup_Location");
+    const DropoffLocation = document.getElementById("Dropoff_Location");
+    const SpecialInstruction = document.getElementById("Special_Instruction");
+    console.log(Crew);
+    
+    const formData = {
+        data : {
+            Job_Id1 : ReqId.getAttribute('data'),
+            Service1: Service.getAttribute('data'),
+            Crew1: Crew.getAttribute("value"),
+            Scheduled_Start_Time1: "",
+            Special_Instructions: "",
+            Scheduled_End_Time:"",
+            Driver1:Driver.getAttribute("value"),
+            Customer:Customer.getAttribute('data')
+        }
+    }
+    console.log(formData);
+    
+
+    var config = {
+        appName: "fab-master-erp1",
+        formName: "Assign_Crew",
+        data: formData
+    }
+    ZOHO.CREATOR.API.addRecord(config).then(function (response) {
+        if (response.code == 3000) {
+            console.log("Record added successfully");
+        }else{
+            console.log(response);
+            
+        }
+    });
+
+
+    // const newFormData = {
+    //     data : {
+    //         Job : 
+    //         Materials : 
+    //         Qty : 
+    //     }
+    // }
+    // var config = {
+    //     appName: "fab-master-erp1",
+    //     formName: "Materials_Required_Job",
+    //     data: formData
+    // }
+    // ZOHO.CREATOR.API.addRecord(config).then(function (response) {
+    //     if (response.code == 3000) {
+    //         console.log("Record added successfully");
+    //     }else{
+    //         console.log(response);
+            
+    //     }
+    // });
+}
--- app/js/maping.js
+++ app/js/maping.js
@@ -58,7 +58,7 @@ const bookingTableDisplay = async (val) => {
     const addService = [];
 
     await val.forEach(async e => {
-        console.log(e);
+        // console.log(e);
 
         let getProperty = [];
         const config = {
@@ -83,7 +83,7 @@ const bookingTableDisplay = async (val) => {
         };
         await ZOHO.CREATOR.API.getAllRecords(config2).then(async response => {
             getBooking = await response.data;
-            console.log(getBooking);
+            // console.log(getBooking);
 
         })
 
@@ -129,7 +129,7 @@ const backToBooking = () => {
     document.querySelector(".jobinner").style.display = "none";
 }
 const jobPage = (val) => {
-    console.log(val);
+    // console.log(val);
     let getProperty = [];
     const config = {
         appName: "fab-master-erp1",
@@ -146,8 +146,8 @@ const jobPage = (val) => {
 }
 
 const displayJobCard = async (Map, active) => {
-    console.log(Map);
-
+    // console.log(Map);
+    let activeJob = 0;
     let card = document.createElement('div');
     card.setAttribute("class", `booking-card ${active}`);
     card.setAttribute("id", Map.ID);
@@ -166,8 +166,8 @@ const displayJobCard = async (Map, active) => {
                 <span class="material-symbols-outlined">map</span> Supervisor
             </li>
         </ul>
-        <div class="job-requests" id="job${Map.Booking_ID}">
-            <div><strong>Job Requests</strong></div>
+        <div><strong>Job Requests</strong></div>
+        <div class="job-requests" id="job${Map.Booking_ID}"></div>
         `;
 
 
@@ -181,29 +181,168 @@ const displayJobCard = async (Map, active) => {
     };
     await ZOHO.CREATOR.API.getAllRecords(config).then(async response => {
         getJobs = await response.data;
-        console.log(getJobs);
-    })
-    getJobs.forEach((e) => {
-        let jobs = document.createElement("div");
-        jobs.innerHTML = `
-            <div class="flex items-center justify-between bg-white text-gray-700 p-3 rounded-md m-2">
-                <div>
-                <p class="font-semibold text-sm">${e.Start_Time.split(" ")[1]} - ${e.End_Time.split(" ")[1]}</p>
-                <p class="text-sm">${e.Service_Type1}</p>
-                </div>
-                <div class="text-sm text-right">
-                <p>Crew A</p>
-                <p>Driver A</p>
-                </div>
-                <div class="bg-green-500 text-white p-1.5 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                </div>
-            </div>    
-        `;
-        card.appendChild(jobs)
+        // console.log(getJobs);
     })
     bookingJobContainer.appendChild(card);
+    let getVal = document.querySelector(`#job${Map.Booking_ID}`);
+    getJobs.forEach(async (e) => {
+        let getCrew = [];
+        let jobs = document.createElement("div");
+        jobs.setAttribute("class", "flex items-center bookingJobs justify-between bg-white text-gray-700 p-3 rounded-md m-2")
+        jobs.setAttribute("id", e.ID);
+        if (activeJob == 0 && active == "active") {
+            updateJobFullDetails(e.ID);
+            activeJob++;
+        }
+        try {
+            const config = {
+                appName: "fab-master-erp1",
+                reportName: "All_Crews",
+                criteria: `(Job_Id1 == ${e.ID})`,
+                page: 1,
+                pageSize: 200,
+            };
+            await ZOHO.CREATOR.API.getAllRecords(config).then(async response => {
+                getCrew = await response.data || [];
+                // console.log(response.data);
+            })
+        } catch (error) {
+            console.log(error.status);
+        }
+        let crew = "--";
+        let driver = "--";
+        if (getCrew.length > 0) {
+            crew = getCrew[0].Crew1.display_value;
+            driver = getCrew[0].Driver1.display_value;
+        }
+        jobs.innerHTML = `
+            <div class="w-full flex flex-wrap">
+                <p class="w-3/5 font-semibold text-sm">${e.Start_Time.split(" ")[1]} - ${e.End_Time.split(" ")[1]}</p> 
+                <p class="w-1/5">${crew}</p> 
+                <p class="w-1/5">${driver}</p> 
+                <p class="w-3/5 text-sm">${e.Job_Name.display_value}</p>
+                <p class="w-1/5>
+                    <i class="fa fa-flag" aria-hidden="true"></i>
+                </p>                
+            </div>   
+        `;
+        getVal.appendChild(jobs)
 
+    })
+
+}
+const updateJobFullDetails = async (jobId) => {
+    console.log(jobId);
+    const ReqId = document.getElementById("Job_Request_ID");
+    const Customer = document.getElementById("Customer");
+    const Service = document.getElementById("Service");
+    const PropertyName = document.getElementById("Property_Name");
+    const PropertyNumber = document.getElementById("Property_Number");
+    const PropertyType = document.getElementById("Property_Type");
+    const PropertyUnit = document.getElementById("Property_Unit");
+    const Address = document.getElementById("Address");
+    const Zone = document.getElementById("Zone");
+    const AccessCode = document.getElementById("Access_Code");
+    const ScheduledDate = document.getElementById("ScheduledDate");
+    const StartTime = document.getElementById("Start_Time");
+    const EndTime = document.getElementById("End_Time");
+    const Crew = document.getElementById("Crew");
+    const Driver = document.getElementById("Driver");
+    const PickupLocation = document.getElementById("Pickup_Location");
+    const DropoffLocation = document.getElementById("Dropoff_Location");
+    const SpecialInstruction = document.getElementById("Special_Instruction");
+    let Checkgrew = false;
+    try {
+        const cre = {
+            appName: "fab-master-erp1",
+            reportName: "All_Crews",
+            criteria: `(Job_Id1 == ${jobId})`,
+            page: 1,
+            pageSize: 200,
+        };
+        await ZOHO.CREATOR.API.getAllRecords(cre).then(async response => {
+            getCrew = await response.data;
+            Checkgrew = true;
+        })
+    } catch (error) {
+        console.log(error);
+    }
+    if (Checkgrew == false) {
+
+        const config = {
+            appName: "fab-master-erp1",
+            reportName: "All_Employees",
+            page: 1,
+            pageSize: 200,
+        };
+        await ZOHO.CREATOR.API.getAllRecords(config).then(async response => {
+            getEmployee = await response.data;
+        })
+        Crew.innerHTML = "<option>Select</option>";
+        Driver.innerHTML = "<option>Select</option>";
+        PickupLocation.innerHTML = "<option>Select</option>";
+        DropoffLocation.innerHTML = "<option>Select</option>";
+        getEmployee.forEach(e => {
+            Crew.innerHTML += `<option vlaue='${e.ID}'>${e.Name}</option>`;
+            Driver.innerHTML += `<option vlaue='${e.ID}'>${e.Name}</option>`;
+        });
+    }
+    else{
+        
+        Crew.innerHTML = `<option value="${getCrew[0].Crew.ID}">${getCrew[0].Crew.display_value}</option>`;
+        Driver.innerHTML = `<option value="${getCrew[0].Driver.ID}">${getCrew[0].Drive.display_value}</option>`;
+        PickupLocation.innerHTML = "<option>Select</option>";
+        DropoffLocation.innerHTML = "<option>Select</option>";
+    }
+
+
+    const config = {
+        appName: "fab-master-erp1",
+        reportName: "All_Jobs",
+        criteria: `(ID == ${jobId})`,
+        page: 1,
+        pageSize: 200,
+    };
+    await ZOHO.CREATOR.API.getAllRecords(config).then(async response => {
+        getJob = await response.data;
+    })
+    console.log(getJob);
+
+    const config2 = {
+        appName: "fab-master-erp1",
+        reportName: "All_Booking_Line_Items",
+        criteria: `(Parent_ID == ${getJob[0].Booking_ID.ID})`,
+        page: 1,
+        pageSize: 200,
+    };
+    await ZOHO.CREATOR.API.getAllRecords(config2).then(async response => {
+        getBooking = await response.data;
+    })
+    console.log(getBooking);
+
+
+    const config3 = {
+        appName: "fab-master-erp1",
+        reportName: "All_Properties",
+        criteria: `(ID == ${getBooking[0].Property_Name.ID})`,
+        page: 1,
+        pageSize: 200,
+    };
+    await ZOHO.CREATOR.API.getAllRecords(config3).then(async response => {
+        getProperty = await response.data;
+    })
+    console.log(getProperty);
+
+    ReqId.value = getJob[0].Job_ID;
+    ReqId.setAttribute("data",getJob[0].ID);
+    Service.value = getJob[0].Job_Name.display_value;
+    Service.setAttribute("data",getJob[0].Job_Name.ID);
+    Customer.value = getBooking[0].Customer.display_value;
+    Customer.setAttribute("data",getBooking[0].Customer.ID);
+    PropertyName.value = getProperty[0].Property_Name;
+    PropertyNumber.value = getProperty[0].Property_Number;
+    PropertyType.value = getProperty[0].Property_Type;
+    PropertyUnit.value = getProperty[0].Bhk_Type.display_value;
+    Zone.value = getProperty[0].Zone.display_value;
+    AccessCode.value = getProperty[0].Number_of_Access_Cards_Keys;
 }
